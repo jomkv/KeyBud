b4f72dff525efc21e9f0e9387508525cb828fa2b
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-08-04 23:22:07 +0800

    feat: create post zod setup

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -6,21 +6,58 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useState, useEffect } from "react";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { DevTool } from "@hookform/devtools";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().nonempty("Title is required"),
+  body: z.string().nonempty("Body is required"),
+});
+
+interface IForm {
+  title: string;
+  body: string;
+}
+
 function CreatePost() {
-  const [title, setTitle] = useState<string>("");
   const [titleChars, setTitleChars] = useState<number>(0);
-  const [body, setBody] = useState<string>("");
 
-  const titleHandleChange = (e: any) => {
-    const val = e.target.value;
+  const form = useForm<IForm>({
+    defaultValues: {
+      title: "",
+      body: "",
+    },
+    resolver: zodResolver(schema),
+  });
+  const { register, handleSubmit, formState, control, setValue, watch } = form;
+  const { errors } = formState;
 
-    if (val.length > 150) {
-      return;
-    }
+  useEffect(() => {
+    register("body", { required: true, minLength: 15 });
+  }, [register]);
 
-    setTitle(val);
-    setTitleChars(val.length);
+  const onEditorStateChange = (editorState: any) => {
+    setValue("body", editorState);
   };
+
+  const editorContent = watch("body");
+
+  const onSubmit = (data: IForm) => {
+    console.log(data);
+  };
+
+  // const titleHandleChange = (e: any) => {
+  //   const val = e.target.value;
+
+  //   if (val.length > 150) {
+  //     return;
+  //   }
+
+  //   setTitle(val);
+  //   setTitleChars(val.length);
+  // };
 
   return (
     <div className="bg-light">
@@ -32,15 +69,19 @@ function CreatePost() {
           color: "white",
         }}
       >
-        <Form className="bg-secondary p-4 mt-5 rounded-3">
+        <Form
+          className="bg-secondary p-4 mt-5 rounded-3"
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <p className="fs-2 fw-bold">Create post</p>
           <Form.Group className="mb-3">
             <Form.Label className="fs-5 fw-medium">Title *</Form.Label>
             <Form.Control
               className="fs-5"
               type="text"
-              value={title}
-              onChange={titleHandleChange}
+              // onChange={titleHandleChange}
+              {...register("title")}
             />
             <div className="w-100 d-flex justify-content-end pe-1">
               <Form.Text
@@ -52,12 +93,13 @@ function CreatePost() {
                 {titleChars}/150
               </Form.Text>
             </div>
+            <Form.Control.Feedback type="invalid">
+              {errors.title?.message}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label className="fs-5 fw-medium">Body *</Form.Label>
             <ReactQuill
-              value={body}
-              onChange={setBody}
               modules={{
                 toolbar: [
                   ["bold", "italic", "underline"],
@@ -65,7 +107,12 @@ function CreatePost() {
                   ["clean"],
                 ],
               }}
+              value={editorContent}
+              onChange={onEditorStateChange}
             />
+            <Form.Control.Feedback type="invalid">
+              {errors.body?.message}
+            </Form.Control.Feedback>
           </Form.Group>
           <div className="w-100 d-flex justify-content-end pe-1">
             <Button
@@ -83,6 +130,7 @@ function CreatePost() {
         </Form>
       </Container>
       <ChatWidget />
+      <DevTool control={control} />
     </div>
   );
 }
