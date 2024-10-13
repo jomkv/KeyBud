025d201d58020b7cb437cd7ba3041c4ea282cdd5
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-13 18:44:11 +0800

    fix: hide comment form if user not logged in

--- src/components/post/Comments.tsx
+++ src/components/post/Comments.tsx
@@ -2,11 +2,13 @@ import { ListGroup } from "react-bootstrap";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
 import CommentForm from "./CommentForm";
 import CommentCard from "./CommentCard";
 import { useGetPostCommentsQuery } from "../../state/slices/commentsApiSlice";
 import { IComment } from "../../@types/commentType";
+import { RootState } from "../../state/store";
 
 interface CommentProps {
   postId: string;
@@ -16,6 +18,7 @@ interface CommentProps {
 const Comments: React.FC<CommentProps> = ({ postId, isComment }) => {
   const navigate = useNavigate();
   const { data: comments, isError } = useGetPostCommentsQuery(postId);
+  const user = useSelector((state: RootState) => state.auth.userInfo);
 
   useEffect(() => {
     if (isError) {
@@ -26,7 +29,7 @@ const Comments: React.FC<CommentProps> = ({ postId, isComment }) => {
 
   return (
     <ListGroup className="list-group-flush">
-      <CommentForm isCommentInit={isComment} postId={postId} />
+      {user && <CommentForm isCommentInit={isComment} postId={postId} />}
 
       {comments?.map((comment: IComment, index: number) => (
         <CommentCard key={index} comment={comment} />
