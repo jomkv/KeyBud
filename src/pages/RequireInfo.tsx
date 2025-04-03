356185b5b2502c361be80c888a5c5f8e91cded9e
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-04-03 15:06:56 +0800

    feat: if no info redirect to set info

--- src/pages/RequireInfo.tsx
+++ src/pages/RequireInfo.tsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react";
 import { useUserContext } from "../context/UserContext";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 
 const RequireInfo = ({ children }: { children: JSX.Element }) => {
   const { user, isLoading, isSuccess } = useUserContext();
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     if (!isLoading && isSuccess) {
-      if (user && (!user?.username || !user?.switchType)) {
+      if (
+        user &&
+        (!user?.username || !user?.switchType) &&
+        location.pathname !== "/set-info"
+      ) {
         navigate("/set-info");
       }
     }
